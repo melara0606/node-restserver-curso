543e875756ba85c0d080a5f0fdcc1632fa654a5c
Edwin Melara <melara0606@gmail.com>
2018-10-12 14:56:41 -0600

    Configurando la varible de entorno para MLAB

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if(process.env.NODE_ENV == 'dev'){
   urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-  urlDB = 'mongodb://cafe:cafe123456@ds231961.mlab.com:31961/curso-cafe-udemy'
+  urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
